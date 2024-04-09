34483886c4bbb27d39b6029bf38134c858e0abf3
kwabla-1 <martinkwablaahedor@gmail.com>
2024-04-09 16:43:37 +0000

    init

--- scripts/script.js
+++ scripts/script.js
@@ -63,121 +63,167 @@
 
 
 
-function validateForm(event) {
-  event.preventDefault();
-  var username = document.getElementById("parmentName").value;
-  if (username == "") {
-      alert("Please enter your username");
-      return false;
-  }
-  return displayModal()
+// function validateForm(event) {
+//   event.preventDefault();
+//   var username = document.getElementById("parmentName").value;
+//   if (username == "") {
+//       alert("Please enter your username");
+//       return false;
+//   }
+//   return displayModal()
+// }
+
+// function displayModal() {
+//   var username = document.getElementById("parmentName").value;
+//   document.getElementById("modalUsername").innerHTML = username;
+//   document.getElementById("paymentModal").style.display = "block";
+// }
+
+// function generate_token(){
+//   // issue a get request to the momo api
+//   // respone you selete the token response
+//   // assigne that to a variable 
+//   // return that variable 
+//   return "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6IjQyYTE1ZWQ3LWVlNWItNGUzYi1hZWExLTEwNzVjYjY0NDIwMSIsImV4cGlyZXMiOiIyMDI0LTA0LTA1VDE3OjAzOjAzLjA2NCIsInNlc3Npb25JZCI6ImNhMzZmMDdmLTc1YmYtNGRiOS1iYzQ0LWU0ZjEyMjYwOGNlNiJ9.NGKkiMGnt2dnuOKG0Jqs1Pdl_diYnxXIqsrgxbW8y9rFs1HO7nUrOYVSUDNnU5oTPZDo55RmFRsZLKBtlxh_aJ5QZZks3XEhGAXKG5OtOeMc3AKEeS6cCP3GwjN182r9b-DVOXz48gOyP4SO4WH53i3NBwgXFeY8mQAd3Ryr9RxhCanJ5NTHYD1HiPmBCK9joWllaEUxKYzQPUfSUgu21IU1F9Acm4ets4jX9Uwo5dCOCMxLJcOFpOcK79hz0cRJzEEWmeVq5l3scM039mku-kMBr4CEy3meSupk6jexfs1bO0rtS15YSlbXjVEdXseP5GCJGT8U0WTLiaN9p-pP4Q"
+// }
+
+// function submitPayment() {
+//   const generate_token = generate_token()
+//   const paymentAmount = document.getElementById("totalAmountToBePaid").value;
+//   const username = "42a15ed7-ee5b-4e3b-aea1-1075cb644201";
+//   const password = "e3a7fbbdd1ce4dd69377290566bbb656"; 
+//   const authorizationToken = generate_token;
+
+//   const requestBody = {
+//     "amount": paymentAmount,
+//     "currency": "EUR",
+//     "externalId": "097411065",
+//     "payer": {
+//     "partyIdType": "MSISDN",
+//     "partyId": "260962217114"
+//     },
+//     "payerMessage": "Meal Subscription",
+//     "payeeNote": "Pay to Subscribe To the selected Meal"
+//   }
+
+//   fetch('https://sandbox.momodeveloper.mtn.com/collection/token/', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Ocp-Apim-Subscription-Key': 'b44728c249c24d8bb11d8b8592f4f5a7',
+//       'X-Target-Environment': 'sandbox',
+//       'X-Reference-Id': '28cffd27-1fc6-4c53-8198-868311ff0c7b',
+//       'Authorization': 'Basic Auth ' + authorizationToken
+//     }
+//   })
+
+
+//   fetch('https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay', {
+//     method: 'POST',
+//     body: JSON.stringify(requestBody),
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Ocp-Apim-Subscription-Key': 'b44728c249c24d8bb11d8b8592f4f5a7',
+//       'X-Target-Environment': 'sandbox',
+//       'X-Reference-Id': '28cffd27-1fc6-4c53-8198-868311ff0c7b',
+//       'Authorization': 'Bearer ' + authorizationToken
+//     }
+//   })
+//   .then(response => {
+//     if (response.ok) {
+//       // Payment submission successful, proceed to next step
+//       return response.json();
+//     } else {
+//       // Handle error response
+//       throw new Error('Payment submission failed');
+//     }
+//   })
+//   .then(data => {
+//     // Step 2: Check if payment was successful
+//     // Assuming your API returns a boolean value indicating payment success
+//     if (data.paymentSuccessful) {
+//       // Payment successful, proceed to next step
+//       return fetch('paymentCheckEndpoint');
+//     } else {
+//       // Payment not successful, handle accordingly
+//       throw new Error('Payment was not successful');
+//     }
+//   })
+//   .then(checkResponse => {
+//     // Step 3: Handle payment check response
+//     if (checkResponse.ok) {
+//       // Payment check successful, show success alert to user
+//       alert("Payment successful!");
+      
+//       // Step 4: Issue request to another API
+//       // Example:
+//       fetch('anotherEndpoint', {
+//         method: 'POST',
+//         body: JSON.stringify({ /* Include necessary information */ }),
+//         headers: {
+//           'Content-Type': 'application/json'
+//         }
+//       })
+//       .then(anotherResponse => {
+//         if (anotherResponse.ok) {
+//           // Handle success response from another API
+//         } else {
+//           // Handle error response from another API
+//         }
+//       })
+//       .catch(error => {
+//         // Handle error from another API request
+//       });
+//     } else {
+//       // Handle error response from payment check
+//       throw new Error('Payment check failed');
+//     }
+//   })
+//   .catch(error => {
+//     // Handle any errors that occurred during the process
+//     alert("Error: " + error.message);
+//   })
+//   .finally(() => {
+//     // Close the modal after payment submission, regardless of outcome
+//     document.getElementById("paymentModal").style.display = "none";
+//   });
+//   // alert("Payment of $" + paymentAmount + " submitted successfully!");
+//   // document.getElementById("paymentModal").style.display = "none";
+// }
+
+
+
+const subscription_key = 'b44728c249c24d8bb11d8b8592f4f5a7'
+const generateUserID = '42a15ed7-ee5b-4e3b-aea1-1075cb644201'
+
+const validateForm = (event,usernumber) => {
+    event.preventDefault();
+    const studentName = document.getElementById("studentName").value;
+    const student_id = document.getElementById("studentID").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const schoolName = document.getElementById("schoolName").value;
+    const mealPlan = document.getElementById("mealPlan").value;
+    const numberOfMeals = document.getElementById("numberOfMeals").value;
+    const address = document.getElementById("address").value;
+    const contactNumber = document.getElementById("contactNumber").value;
+    const totalAmount = document.getElementById("totalAmount").value;
+    const payment_type = document.getElementById("payment_type").value;
+   
+    if (studentName == "") {
+        alert("Please enter your username");
+        return false;
+    }
+
+    return displayModal()
 }
 
-function displayModal() {
-  var username = document.getElementById("parmentName").value;
+const displayModal = () => {
+  const username = document.getElementById("studentName").value;
   document.getElementById("modalUsername").innerHTML = username;
   document.getElementById("paymentModal").style.display = "block";
 }
 
-function submitPayment() {
-  const paymentAmount = document.getElementById("totalAmountToBePaid").value;
-  const username = "42a15ed7-ee5b-4e3b-aea1-1075cb644201";
-  const password = "e3a7fbbdd1ce4dd69377290566bbb656"; 
-  const authorizationToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6IjQyYTE1ZWQ3LWVlNWItNGUzYi1hZWExLTEwNzVjYjY0NDIwMSIsImV4cGlyZXMiOiIyMDI0LTA0LTA1VDE3OjAzOjAzLjA2NCIsInNlc3Npb25JZCI6ImNhMzZmMDdmLTc1YmYtNGRiOS1iYzQ0LWU0ZjEyMjYwOGNlNiJ9.NGKkiMGnt2dnuOKG0Jqs1Pdl_diYnxXIqsrgxbW8y9rFs1HO7nUrOYVSUDNnU5oTPZDo55RmFRsZLKBtlxh_aJ5QZZks3XEhGAXKG5OtOeMc3AKEeS6cCP3GwjN182r9b-DVOXz48gOyP4SO4WH53i3NBwgXFeY8mQAd3Ryr9RxhCanJ5NTHYD1HiPmBCK9joWllaEUxKYzQPUfSUgu21IU1F9Acm4ets4jX9Uwo5dCOCMxLJcOFpOcK79hz0cRJzEEWmeVq5l3scM039mku-kMBr4CEy3meSupk6jexfs1bO0rtS15YSlbXjVEdXseP5GCJGT8U0WTLiaN9p-pP4Q";
-
-  const requestBody = {
-    "amount": paymentAmount,
-    "currency": "EUR",
-    "externalId": "097411065",
-    "payer": {
-    "partyIdType": "MSISDN",
-    "partyId": "260962217114"
-    },
-    "payerMessage": "Meal Subscription",
-    "payeeNote": "Pay to Subscribe To the selected Meal"
-  }
-
-  fetch('https://sandbox.momodeveloper.mtn.com/collection/token/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Ocp-Apim-Subscription-Key': 'b44728c249c24d8bb11d8b8592f4f5a7',
-      'X-Target-Environment': 'sandbox',
-      'X-Reference-Id': '28cffd27-1fc6-4c53-8198-868311ff0c7b',
-      'Authorization': 'Basic Auth ' + authorizationToken
-    }
-  })
-
-
-  fetch('https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay', {
-    method: 'POST',
-    body: JSON.stringify(requestBody),
-    headers: {
-      'Content-Type': 'application/json',
-      'Ocp-Apim-Subscription-Key': 'b44728c249c24d8bb11d8b8592f4f5a7',
-      'X-Target-Environment': 'sandbox',
-      'X-Reference-Id': '28cffd27-1fc6-4c53-8198-868311ff0c7b',
-      'Authorization': 'Bearer ' + authorizationToken
-    }
-  })
-  .then(response => {
-    if (response.ok) {
-      // Payment submission successful, proceed to next step
-      return response.json();
-    } else {
-      // Handle error response
-      throw new Error('Payment submission failed');
-    }
-  })
-  .then(data => {
-    // Step 2: Check if payment was successful
-    // Assuming your API returns a boolean value indicating payment success
-    if (data.paymentSuccessful) {
-      // Payment successful, proceed to next step
-      return fetch('paymentCheckEndpoint');
-    } else {
-      // Payment not successful, handle accordingly
-      throw new Error('Payment was not successful');
-    }
-  })
-  .then(checkResponse => {
-    // Step 3: Handle payment check response
-    if (checkResponse.ok) {
-      // Payment check successful, show success alert to user
-      alert("Payment successful!");
-      
-      // Step 4: Issue request to another API
-      // Example:
-      fetch('anotherEndpoint', {
-        method: 'POST',
-        body: JSON.stringify({ /* Include necessary information */ }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(anotherResponse => {
-        if (anotherResponse.ok) {
-          // Handle success response from another API
-        } else {
-          // Handle error response from another API
-        }
-      })
-      .catch(error => {
-        // Handle error from another API request
-      });
-    } else {
-      // Handle error response from payment check
-      throw new Error('Payment check failed');
-    }
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the process
-    alert("Error: " + error.message);
-  })
-  .finally(() => {
-    // Close the modal after payment submission, regardless of outcome
-    document.getElementById("paymentModal").style.display = "none";
-  });
-  // alert("Payment of $" + paymentAmount + " submitted successfully!");
-  // document.getElementById("paymentModal").style.display = "none";
+const make_payment = () => {
+  
 }
